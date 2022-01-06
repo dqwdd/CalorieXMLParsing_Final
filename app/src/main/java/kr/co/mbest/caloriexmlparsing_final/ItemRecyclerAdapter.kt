4e582348225cf70e7d553ddc9ccbb6751c3ff2cc
DESKTOP-PT5UGB4\MEGA <dqwdd@megastudy.net>
2022-01-06 16:11:45 +0900

    2개 뷰 홀더 쓰기 예제 item이랑 어댑터만 손보면 됨

--- app/src/main/java/kr/co/mbest/caloriexmlparsing_final/ItemRecyclerAdapter.kt
+++ app/src/main/java/kr/co/mbest/caloriexmlparsing_final/ItemRecyclerAdapter.kt
@@ -1,6 +1,7 @@
 package kr.co.mbest.caloriexmlparsing_final
 
 import android.content.Context
+import android.util.EventLogTags
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,30 +9,76 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.apipractice.json.Item
 
+private const val Big = 0
+private const val Small = 1
 
 class ItemRecyclerAdapter(
     val mContext: Context,
     val mList: List<Item>) :
-    RecyclerView.Adapter<ItemRecyclerAdapter.ItemViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+
+    class BigViewHolder(val mContext: Context, view : View) : RecyclerView.ViewHolder(view) {
+        val textView = view.findViewById<TextView>(R.id.big_tv_desk_kor)
+        fun bind( context: Context, data: Item ) {
+            textView.text =data.DESC_KOR
+        }
+    }
+
+    class SmallViewHolder(val mContext: Context, view : View) : RecyclerView.ViewHolder(view) {
+        val textView = view.findViewById<TextView>(R.id.tv_desk_kor)
+        fun bind( context: Context, data: Item ) {
+            textView.text =data.BGN_YEAR
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == Big) {
+            val view = LayoutInflater.from(mContext).inflate(R.layout.item_colorie_data_list_big, parent, false)
+            return BigViewHolder(mContext, view)
+        }
+        else {
+            val view = LayoutInflater.from(mContext).inflate(R.layout.item_colorie_data_list, parent, false)
+            return SmallViewHolder(mContext, view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(getItemViewType(position) == Big) {
+            (holder as BigViewHolder).bind(mContext, mList[position])
+        }
+        else {
+            (holder as SmallViewHolder).bind(mContext, mList[position])
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if(mList[0] == mList[position]) { Big }
+        else { Small }
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+
+}
+
+/*
     class ItemViewHolder(val mContext: Context, view : View) : RecyclerView.ViewHolder(view) {
 
         val textView = view.findViewById<TextView>(R.id.tv_desk_kor)
-        val textView2 = view.findViewById<TextView>(R.id.tv_serving_wt)
-        val textView3 = view.findViewById<TextView>(R.id.tv_nutr_cont1)
-        val textView4 = view.findViewById<TextView>(R.id.tv_nutr_cont3)
 
         fun bind( context: Context, data: Item ) {
             textView.text =data.DESC_KOR
-            textView2.text =data.SERVING_WT
-            textView3.text =data.NUTR_CONT1
-            textView4.text =data.NUTR_CONT3
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val view = LayoutInflater.from(mContext).inflate(R.layout.item_colorie_data_list, parent, false)
+        if (mList.)
+        val view = LayoutInflater.from(mContext).inflate(R.layout.item_colorie_data_list_big, parent, false)
         return ItemViewHolder(mContext, view)
     }
 
@@ -43,5 +90,4 @@ class ItemRecyclerAdapter(
     override fun getItemCount(): Int {
         return mList.size
     }
-
-}
+ */
